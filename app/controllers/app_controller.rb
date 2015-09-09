2125f24c2dbc9258af25651200e1acd027c9b80a
Colin Treseler <treseler@gmail.com>
2015-09-09 06:53:43 +0200

    app_controller

---
+++ app/controllers/app_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ApplicationController
+  acts_as_token_authentication_handler_for User
+  before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+end
