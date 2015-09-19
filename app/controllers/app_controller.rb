c87cc9a1c56674c7858b6ddd7f434952e2d88a35
Colin Treseler <treseler@gmail.com>
2015-09-19 21:10:11 +0200

    command_line api
    models for container and project

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -2,4 +2,23 @@ class AppController < ApplicationController
   acts_as_token_authentication_handler_for User, fallback: :exception
   before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
+
+  def command_line
+    if params[:command]
+      puts 'command here'
+      init_rails_project if params[:command].include?("rails new")
+      render json: {
+        output: "your rails app has been created"
+      }
+    else
+    render json: {
+      output: "output string from the rails container"
+    }
+    end
+  end
+
+  private
+
+  def init_rails_project
+  end
 end
---
+++ app/models/container.rb
@@ -0,0 +1,8 @@
+class Container < ActiveRecord::Base
+  belongs_to :user
+
+  def init_data_volume
+    
+  end
+
+end
