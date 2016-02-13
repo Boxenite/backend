cdeae687683ebed4f40b13a13d709b6bcfc4d440
Colin Treseler <treseler@gmail.com>
2016-02-13 19:24:25 +0100

    cleanup and full names

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: :sessions }
+  devise_for :users
   root to: 'dashboard#index'
 
   get "home" => "pages#home", as: :home
